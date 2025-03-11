fa757d668fac2be08ef2d45b66cdcfb3376feff5
ziurejimas <nojus.kondrotas@ktu.edu>
2025-03-11 21:03:14 +0200

    define opcodes

--- annuli.c
+++ annuli.c
@@ -19,3 +19,23 @@ enum
 };
 
 uint16_t reg[R_COUNT];
+
+enum
+{
+    OP_BR = 0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+}
